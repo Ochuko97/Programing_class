bf4f1d5365def39e793fc0158ae5badd3882446d
Ochuko97 <ochuko97@hotmail.com>
2016-10-07 14:37:38 +0100

    Add files via upload

---
+++ Ochuko_programing/Pyrimaid/Pyrimaid/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyrimaid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = 0;
+            bool valid = false;
+
+            while (valid == false) 
+            { 
+                Console.Write("Enter the pyrimid height: ");
+                string currentEntry = Console.ReadLine();
+                valid = Int32.TryParse(currentEntry, out height);
+
+                if (height <= 0 || height > 23 )
+                    valid = false;
+            }
+
+            int hashCount = 2;
+
+            for (int i = 0; i < height; i++)
+            {
+                string line = "";
+                int spaces = height - hashCount + 1;
+
+                for (int j = 0; j < spaces; j++)
+                {
+                    line += " ";
+                }
+
+                for (int j = 0; j < hashCount; j++)
+                {
+                    line += "#"; 
+                }
+                Console.WriteLine(line);
+                hashCount++;
+            }
+            Console.ReadLine();           
+        }
+        
+    }
+}
